a002234464c8a17560a78323252599f7c8f8e8a5
bjoernzingel <31500532+bjoernzingel@users.noreply.github.com>
2019-10-11 10:35:29 +0200

    Update auth.go

--- backends/refresh/auth.go
+++ backends/refresh/auth.go
@@ -40,8 +40,8 @@ import (
 	"gopkg.in/yaml.v2"
 
 	"github.com/allegro/bigcache"
-	"github.com/freman/caddy-reauth/backend"
-	secrets "github.com/freman/caddy-reauth/lib/caddy-secrets"
+	"github.com/bjoernzingel/caddy-reauth/backend"
+	secrets "github.com/bjoernzingel/caddy-reauth/lib/caddy-secrets"
 )
 
 // Backend name
